23e4ec026dbddee4c3741ec642675c3afd98d301
wolf <cory.klokman@ibm.com>
2021-06-04 16:10:08 -0500

    Changing variables to powervs_
    Changing variables to powervs_, added a tag ${var.cluster_id} to the iaas service, and changed the provider version to 1.24.0
    
    Signed-off-by: wolf <cory.klokman@ibm.com>

--- data/data/powervs/iaas/power-iaas.tf
+++ data/data/powervs/iaas/power-iaas.tf
@@ -1,17 +1,17 @@
 provider "ibm" {
- ibmcloud_api_key = var.ibmcloud_api_key
+ ibmcloud_api_key = var.powervs_api_key
 }
 
 data "ibm_resource_group" "group" { 
-  name = var.cloud_resource_group
+  name = var.powervs_resource_group
 }
 
 resource "ibm_resource_instance" "resource_instance" { 
   name     = "${var.cluster_id}-power-iaas" 
   service  = "power-iaas"
   plan     = "power-virtual-server-group" 
-  location = var.ibmcloud_region
-  tags     = concat( var.service_tags, [ "${var.cluster_id}-power-iaas" ] )
+  location = var.powervs_region
+  tags     = concat( var.service_tags, [ "${var.cluster_id}-power-iaas", "${var.cluster_id}" ] )
   resource_group_id = data.ibm_resource_group.group.id 
   
   timeouts { 
