6c56836fb25e79db200495d57b8bb55a6efb8823
Karthik K N <karthikkn1997@gmail.com>
2021-06-23 12:57:31 +0530

    Changed the default PowerVS machine processor value

--- pkg/asset/machines/worker.go
+++ pkg/asset/machines/worker.go
@@ -159,7 +159,7 @@ func defaultKubevirtMachinePoolPlatform() kubevirttypes.MachinePool {
 func defaultPowerVSMachinePoolPlatform() powervstypes.MachinePool {
 	return powervstypes.MachinePool{
 		Memory:     32,
-		Processors: 0.25,
+		Processors: 0.5,
 		ProcType:   "shared",
 		SysType:    "s922",
 	}
