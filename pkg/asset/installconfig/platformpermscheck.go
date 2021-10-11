cad1a811af7867149ef26b5a594b1b36be5b58b7
rna-afk <anarayan@redhat.com>
2021-10-11 12:47:59 -0400

    aws: Remove shared tags from user specified IAM roles
    The installer tags the IAM roles that are specified by the user
    and deletes it during cluster destroy which is not ideal since
    the installer should not be deleting the user created resources.
    
    Removing the code that tags the IAM roes provided by the user and
    also removing the permission required to delete the IAM role if
    the user provided it.

--- pkg/asset/installconfig/platformpermscheck.go
+++ pkg/asset/installconfig/platformpermscheck.go
@@ -10,9 +10,6 @@ import (
 	awsconfig "github.com/openshift/installer/pkg/asset/installconfig/aws"
 	gcpconfig "github.com/openshift/installer/pkg/asset/installconfig/gcp"
 	kubevirtconfig "github.com/openshift/installer/pkg/asset/installconfig/kubevirt"
-
-	//powervsconfig "github.com/openshift/installer/pkg/asset/installconfig/powervs"
-	"github.com/openshift/installer/pkg/types"
 	"github.com/openshift/installer/pkg/types/aws"
 	"github.com/openshift/installer/pkg/types/azure"
 	"github.com/openshift/installer/pkg/types/baremetal"
@@ -70,9 +67,6 @@ func (a *PlatformPermsCheck) Generate(dependencies asset.Parents) error {
 			} else {
 				permissionGroups = append(permissionGroups, awsconfig.PermissionDeleteNetworking)
 			}
-			if awsIncludesUserSuppliedInstanceRole(ic.Config) {
-				permissionGroups = append(permissionGroups, awsconfig.PermissionDeleteSharedInstanceRole)
-			}
 		}
 
 		ssn, err := ic.AWS.Session(ctx)
@@ -118,22 +112,4 @@ func (a *PlatformPermsCheck) Generate(dependencies asset.Parents) error {
 // Name returns the human-friendly name of the asset.
 func (a *PlatformPermsCheck) Name() string {
 	return "Platform Permissions Check"
-}
-
-func awsIncludesUserSuppliedInstanceRole(installConfig *types.InstallConfig) bool {
-	mp := &aws.MachinePool{}
-	mp.Set(installConfig.Platform.AWS.DefaultMachinePlatform)
-	mp.Set(installConfig.ControlPlane.Platform.AWS)
-	if mp.IAMRole != "" {
-		return true
-	}
-	for _, c := range installConfig.Compute {
-		mp := &aws.MachinePool{}
-		mp.Set(installConfig.Platform.AWS.DefaultMachinePlatform)
-		mp.Set(c.Platform.AWS)
-		if mp.IAMRole != "" {
-			return true
-		}
-	}
-	return false
 }
