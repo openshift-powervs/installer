shiftstack/openshift-installer

openstack: Expose worker server group policy
With this change, Compute nodes within each MachineSet are automatically
created in a Server group, with a default policy of
"soft-anti-affinity".

With this change, a "serverGroupPolicy" can be set in install-config, on
the worker MachinePool and/or in the platform default.

Implements OSASINFRA-2570

Co-Authored-By: Matthew Booth <mbooth@redhat.com>

data/data/openstack/variables-openstack.tf
@@ -376,3 +376,13 @@ variable "openstack_master_root_volume_availability_zones" {
   default = [""]
   description = "List of availability Zones to Schedule the masters root volumes on."
 }
+
+variable "openstack_worker_server_group_names" {
+  type = set(string)
+  description = "Names of the server groups for the worker nodes."
+}
+
+variable "openstack_worker_server_group_policy" {
+  type = string
+  description = "Policy of the server group for the worker nodes."
+}

data/data/openstack/masters/variables.tf
@@ -65,3 +65,13 @@ variable "root_volume_zones" {
   type        = list(string)
   description = "Availability Zones to schedule root volumes on."
 }
+
+variable "worker_server_group_names" {
+  type        = set(string)
+  description = "Names of the server groups for the worker nodes."
+}
+
+variable "worker_server_group_policy" {
+  type        = string
+  description = "Policy of the server group for the worker nodes."
+}

openshift-powervs/installer

data/data/openstack/variables-openstack.tf
@@ -364,3 +364,13 @@ variable "openstack_master_root_volume_availability_zones" {
   default     = [""]
   description = "List of availability Zones to Schedule the masters root volumes on."
 }
+
+variable "openstack_worker_server_group_names" {
+  type        = set(string)
+  description = "Names of the server groups for the worker nodes."
+}
+
+variable "openstack_worker_server_group_policy" {
+  type        = string
+  description = "Policy of the server groups for the worker nodes."
+}

data/data/openstack/workers/variables.tf
@@ -1,0 +1,9 @@
+variable "server_group_names" {
+  type        = set(string)
+  description = "Names of the server groups for the worker nodes."
+}
+
+variable "server_group_policy" {
+  type        = string
+  description = "Policy of the server groups for the worker nodes."
+}