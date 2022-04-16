e4cf051cf894bfb765bdaefb80291453a9e9615d
Christy Norman <christy@linux.vnet.ibm.com>
2022-04-15 23:26:56 -0500

    tf timing and dependency fixes
    30s is not enough for the dhcp ips, and the load balancer members also need some dependency updates
    
    Signed-off-by: Christy Norman <christy@linux.vnet.ibm.com>

--- data/data/powervs/cluster/master/lb/variables.tf
+++ data/data/powervs/cluster/master/lb/variables.tf
@@ -5,3 +5,7 @@ variable "lb_int_id" {}
 variable "machine_cfg_pool_id" {}
 variable "api_pool_int_id" {}
 variable "api_pool_ext_id" {}
+
+# only used for dependency reasons
+variable "bootstrap_api_member_int_id" {}
+variable "bootstrap_api_member_ext_id" {}
