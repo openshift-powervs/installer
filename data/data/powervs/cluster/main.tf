4367e301d85d6c5363679d60e467a726710445b1
Christy Norman <christy@linux.vnet.ibm.com>
2022-08-01 13:44:44 -0700

    fix apikey var
    Signed-off-by: Christy Norman <christy@linux.vnet.ibm.com>

--- data/data/powervs/cluster/main.tf
+++ data/data/powervs/cluster/main.tf
@@ -1,6 +1,6 @@
 provider "ibm" {
   alias            = "vpc"
-  ibmcloud_api_key = var.ibmcloud_api_key
+  ibmcloud_api_key = var.powervs_ibmcloud_api_key
   region           = var.powervs_vpc_region
   zone             = var.powervs_vpc_zone
 }
@@ -53,7 +53,7 @@ module "master" {
   resource_group    = var.powervs_resource_group
   instance_count    = var.master_count
 
-  ibmcloud_api_key    = var.ibmcloud_api_key
+  ibmcloud_api_key    = var.powervs_ibmcloud_api_key
   powervs_api_key     = var.powervs_api_key
   powervs_region      = var.powervs_region
   powervs_zone        = var.powervs_zone
